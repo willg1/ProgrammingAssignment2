2380b06cb474e2683c2fec9f6a4eb6d8ecb4e001
william <williamgalloway@optimum.net>
2015-01-25 13:42:14 -0500

    Added Function and push to Repository

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
-## Write a short comment describing this function
-
+ ## Return a matrix that is the inverse of 'x'
+##Create matrix inverse, self
 makeCacheMatrix <- function(x = matrix()) {
-
+  n <- NULL
+  key <- function(z){
+    x <<- z
+    n <<- NULL
+  }
+  put  <- function() x 
+  lessinverse  <- function(inverse) n  <<- inverse 
+  putinverse  <- function() n 
+  list(less= less, put = put,  
+       lessinverse = lessinverse,  
+       putinverse = getinverse) 
 }
 
 
-## Write a short comment describing this function
+## Second Funtion inverse cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Matrix of x is return inverse of'x'
+  n  <- x$putinverse() 
+  if (!is.null(n)){ 
+    message("data being cache") 
+    return(i) 
+  } 
+  data  <- x$put() 
+  n  <- solve(data, ...) 
+  x$setinverse(n) 
+  n 
 }
